686b93833a1bbbddab65c66f8eb37939594ebc92
Pablo Loaiza <pablo@loaizas.com>
2021-02-15 15:22:22 -0500

    ejemplo_2

---
+++ ejemplo2/Variables.java
@@ -0,0 +1,18 @@
+import javax.print.event.PrintEvent;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte dia; // declaración
+        byte a,b,c; // declaración
+        int d;
+
+        dia = 31; // inicialización
+        a = 60;
+        b = 40;
+
+        d = a + b;
+
+        //System.out.println("El dia es " + dia);
+        System.out.println("la suma es " + d);
+    }
+}
