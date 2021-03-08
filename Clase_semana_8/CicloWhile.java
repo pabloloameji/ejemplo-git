a03466f603bd684b8d1dd8aba8f5deee8b65cd93
Pablo Loaiza <pablo@loaizas.com>
2021-03-08 15:41:03 -0500

    clase lunes semana 8

---
+++ Clase_semana_8/CicloWhile.java
@@ -0,0 +1,11 @@
+class CicloWhile {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while (contador <= 10) {
+            contador ++;
+        } 
+
+        System.out.println(contador);
+    }
+}
