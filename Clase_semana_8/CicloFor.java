c605dc971a37af2ef92df1a4442d199755c576ca
Pablo Loaiza <pablo@loaizas.com>
2021-03-10 15:52:01 -0500

    clase miercoles semana 8

---
+++ Clase_semana_8/CicloFor.java
@@ -0,0 +1,19 @@
+class CicloFor {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 1; i < 101; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println(" ");
+
+        for (i = 250; i < 1201; i++) {
+            System.out.print(i + "  ");
+        }
+
+        System.out.println(" ");
+
+        
+    }
+}
